9a84acc0886f2309d1b0d1035803caf24e9b1695
Albert-Mary Dorcé <dorce@MATINF.uqtr.ca>
2022-03-04 15:27:43 -0500

    I created a new to-do-list with bootstrap

---
+++ build/precache-manifest.6a849e0be806a537675fd226c09bbfb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17aa079176a82fef66f52c684a3d2198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329797fe274473d2d0f4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b257f3564ee4e7fa53b0",
+    "url": "/static/css/main.9cbaddb6.chunk.css"
+  },
+  {
+    "revision": "329797fe274473d2d0f4",
+    "url": "/static/js/2.6bb7e379.chunk.js"
+  },
+  {
+    "revision": "b257f3564ee4e7fa53b0",
+    "url": "/static/js/main.700f7513.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import configureStore from './store/store'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/toDo.css'
+
+const store = conf
